9cf3796cfca2f99c5984be4824e058b3fcaa0a9f
LukasBrondel <lukas_broendel@hotmail.com>
2018-11-28 15:31:24 +0100

    Work on index

---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light customNavbar">
+  <a class="navbar-brand" href="#">[LOGO]</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="nav navbar-nav ml-auto ">
+      <li class="nav-item custom-menu-links">
+        <a class="nav-link" href="#">Forside</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Find job</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Appen</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Kunder</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Kontakt</a>
+      </li>
+      <li class="nav-item login-btn">
+        <a class="nav-link login-link" href="#">Login</a>
+      </li>
+    </ul>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- Custom stylesheet minified -->
+	<link href="css/style.min.css" rel="stylesheet">
+    
+    <title>Studiejobs.dk</title>
+  </head>
+  <body>
+ 
+        <?php include 'includes/header.php';?>
+        
+    <section class="container-fluid intro-slider">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="images/wolt-featured.jpg" alt="Udbringer hos Wolt">
+              <div class="carousel-caption d-none d-md-block">
+                <h1>Savner du ekstra lommepenge?</h1>
+                <p>Som leverandør for Wolt får du selv mulighed for at styre og planlægge dine arbejdstider efter dine behov. Det eneste krav er, at du har en cykel og en smartphone!</p>
+                <a href="#" class="slider-btn">Læs mere</a>
+              </div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+    </section>
+    <section class="container">
+        <h2>Nye jobopslag</h2>
+        <div class="separator"><span>X</span></div>
+        
+        <article class="row search-job">
+            <form class="form-inline">
+              <div class="form-group">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Postnummer / by">
+              </div>
+              <div class="form-group">
+                <select class="form-control" id="exampleFormControlSelect1" placeholder="Jobtype">
+                    <option>Alle opslag</option>
+                    <option>Studiejobs</option>
+                    <option>Praktik</option>
+                </select>
+              </div>
+            <button type="submit" class="btn btn-default">Søg job</button>
+            </form>
+        </article>
+        <article class="row nye-jobs">
+            <table class="table">
+              <tbody>
+                <tr>
+                  <td>
+                    <div class="job-logo">
+                        <img src="images/virksomheder/fk-distribution-logo.png" alt="FK Distribution logo">
+                    </div>
+                  </td>
+                  <td>
+                      <h3>Omdeler</h3>
+                      <p>FK Distribution</p>
+                  </td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <td>
+                    <div class="job-logo">
+                        <img src="images/virksomheder/fk-distribution-logo.png" alt="FK Distribution logo">
+                    </div>
+                  </td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                  <td>@mdo</td>
+                </tr>
+              </tbody>
+            </table>
+        </article>
+    </section>  
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
